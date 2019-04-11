beaab0d2cc0c1115a4b6e5c5238f6029f05c5f8a
Daniel Thonon <d.thonon9@gmail.com>
2019-04-11 08:41:29 +0200

    Revert "Merge branch 'develop' into 'master'"
    This reverts merge request !20

--- export_vn/setup.py
+++ export_vn/setup.py
@@ -1,61 +1,30 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 """Setup for export_vn package.
 """
-from setuptools import find_packages, setup
-from setuptools_scm import get_version
-
-setup_requires = []
-install_requires = [
-    'docutils>=0.3'
-    "requests",
-]
-test_requires = [
-    "pytest",
-    "pylint",
-    "coverage",
-    "setuptools_scm",
-    "pytest-pylint",
-    "pytest-cov",
-]
-docs_requires = [
-    "sphinx"
-]
-dev_requires = docs_requires + test_requires
+from setuptools import setup, find_packages
 
 setup(
     name="export_vn",
-    python_requires=">=3.6",
-
-    setup_requires=setup_requires,
-    install_requires=install_requires,
-
-    use_scm_version={'root': '..', 'relative_to': __file__},
-    version=get_version(root='..', relative_to=__file__),
-
     packages=find_packages(),
     include_package_data=True,
-    extras_require={"docs": docs_requires,
-                    "tests": test_requires,
-                    "dev": dev_requires},
+
+    use_scm_version={'root': '..', 'relative_to': __file__},
+    setup_requires=['setuptools_scm'],
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=['docutils>=0.3'],
 
     package_data={
         # If any package contains *.txt or *.rst files, include them:
         '': ['*.txt', '*.rst'],
-        # And include any *.msg files too:
+        # And include any *.msg files found in the 'hello' package, too:
         'export_vn': ['*.msg'],
     },
 
     # metadata to display on PyPI
     author="Daniel Thonon",
     author_email="d.thonon9@gmail.com",
-    description="Download data from VisioNature and store to postgresql database",
+    description="",
     license="GPL",
-
-    classifiers=[
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: Debian 9',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-    ],
 )
