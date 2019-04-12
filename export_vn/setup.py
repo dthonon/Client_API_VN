ac178a0e58e7efd96232f2d946dff03f80fe3ce2
Daniel Thonon <d.thonon9@gmail.com>
2019-04-12 02:55:02 +0200

    Updated setup.py with dependencies and misc updates. Removed deprecated test in INSTALL.md

--- export_vn/setup.py
+++ export_vn/setup.py
@@ -3,6 +3,9 @@
 """
 from setuptools import setup, find_packages
 
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="export_vn",
     packages=find_packages(),
@@ -13,18 +16,30 @@ setup(
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires=['docutils>=0.3'],
+    install_requires=[
+        'beautifulsoup4>=4.5',
+        'pyexpander>=1.8'
+        'pyproj>=1.9',
+        'requests>=2.12',
+        'requests-oauthlib>=0.7',
+        'SQLAlchemy>=1.0',
+        'tabulate>=0.7',
+        'xmltodict>=0.10'
+    ],
 
     package_data={
-        # If any package contains *.txt or *.rst files, include them:
-        '': ['*.txt', '*.rst'],
-        # And include any *.msg files found in the 'hello' package, too:
-        'export_vn': ['*.msg'],
-    },
+     },
 
     # metadata to display on PyPI
     author="Daniel Thonon",
     author_email="d.thonon9@gmail.com",
-    description="",
+    description="Transfer data from VisioNature web site to Postgresql database",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="GPL",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL",
+        "Operating System :: OS Independent",
+    ],
 )
