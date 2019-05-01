5d837d924e8a712ccc8961fbe3f625e5855ec4cc
Daniel Thonon <d.thonon9@gmail.com>
2019-05-01 19:51:18 +0200

    Still trying to upload with scm

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 """Setup for export_vn package.
 """
+import os
 from setuptools import setup, find_packages
 
 import setuptools
 
 if __name__ == "__main__":
     setuptools.setup(
-        use_scm_version=True
+        use_scm_version={'relative_to': os.path.dirname(__file__)}
     )
