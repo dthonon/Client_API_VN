20ec82d0a064c2dbd8b102e0f656e8e6e3793962
Daniel Thonon <d.thonon9@gmail.com>
2019-05-08 07:49:54 +0200

    Cleanup code

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@
 """Setup for export_vn package.
 """
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
-import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup(
+    setup(
         use_scm_version={'relative_to': os.path.dirname(__file__)}
     )
