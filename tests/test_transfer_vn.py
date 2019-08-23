d51221d41199a1839b05bb3f2b8cc6a5cc5b42de
Daniel Thonon <d.thonon9@gmail.com>
2019-08-24 01:11:17 +0200

    Improved test coverage, Issue #36

--- tests/test_transfer_vn.py
+++ tests/test_transfer_vn.py
@@ -1,13 +1,41 @@
 """
 Test transfer_vn main.
 """
-import logging
+from pathlib import Path
 from unittest.mock import patch
 
+import pytest
 from export_vn import transfer_vn
 
 
 def test_version():
     """Check if version is defined."""
+    with patch("sys.argv", ["py.test", "--version"]):
+        with pytest.raises(SystemExit):
+            transfer_vn.run()
+
+
+def test_init():
+    """Check --init parameter."""
+    file_yaml = str(Path.home()) + "/" + ".evn_pytest.yaml"
+    if Path(file_yaml).is_file():
+        Path(file_yaml).unlink()
     with patch("sys.argv", ["py.test", "--init", ".evn_pytest.yaml"]):
+        transfer_vn.run()
+    assert Path(file_yaml).is_file()
+
+
+@pytest.mark.slow
+def test_db_ops():
+    """Check database management parameters."""
+    file_yaml = ".evn_test.yaml"
+    with patch("sys.argv", ["py.test", "--db_drop", file_yaml]):
+        transfer_vn.run()
+    with patch("sys.argv", ["py.test", "--db_create", file_yaml]):
+        transfer_vn.run()
+    with patch("sys.argv", ["py.test", "--json_tables_create", file_yaml]):
+        transfer_vn.run()
+    with patch("sys.argv", ["py.test", "--col_tables_create", file_yaml]):
+        transfer_vn.run()
+    with patch("sys.argv", ["py.test", "--full", file_yaml]):
         transfer_vn.run()
