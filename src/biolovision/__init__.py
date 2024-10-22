d5a78399306b1dd5eb6745d92bb1793f4b413bb3
Daniel Thonon <d.thonon9@gmail.com>
2024-10-23 00:19:09 +0200

    make install sans erreur

---
+++ src/biolovision/__init__.py
@@ -0,0 +1,19 @@
+import gettext
+from pathlib import Path
+
+from pkg_resources import DistributionNotFound, get_distribution
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = "Client_API_VN"
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:  # pragma: no cover
+    __version__ = "unknown"
+finally:
+    del get_distribution, DistributionNotFound
+
+# Install gettext for any file in the application
+localedir = Path(__file__).resolve().parent / "locale"
+gettext.bindtextdomain("export_vn", str(localedir))
+gettext.textdomain("export_vn")
+_ = gettext.gettext
