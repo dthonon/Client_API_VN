eb34272cb77d56da7e5e598f7adae8b742d7ced5
Daniel Thonon <d.thonon9@gmail.com>
2024-10-25 23:37:58 +0200

    Tests initialisés. Reste à ajouter les autres modules

--- src/biolovision/__init__.py
+++ src/biolovision/__init__.py
@@ -1,17 +1,18 @@
 import gettext
 from pathlib import Path
 
-from pkg_resources import DistributionNotFound, get_distribution
+# from pkg_resources import DistributionNotFound, get_distribution
 
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = "Client_API_VN"
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:  # pragma: no cover
-    __version__ = "unknown"
-finally:
-    del get_distribution, DistributionNotFound
+# try:
+#     # Change here if project is renamed and does not equal the package name
+#     dist_name = "Client_API_VN"
+#     __version__ = get_distribution(dist_name).version
+# except DistributionNotFound:  # pragma: no cover
+#     __version__ = "unknown"
+# finally:
+#     del get_distribution, DistributionNotFound
 
+__version__ = "unknown"
 # Install gettext for any file in the application
 localedir = Path(__file__).resolve().parent / "locale"
 gettext.bindtextdomain("export_vn", str(localedir))
