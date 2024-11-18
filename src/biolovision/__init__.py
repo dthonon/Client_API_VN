0edd14c62621f4d5623d66aa274aa9075e47b718
Daniel Thonon <d.thonon9@gmail.com>
2024-11-18 23:13:15 +0100

    Remove depracated toml Fixes #353

--- src/biolovision/__init__.py
+++ src/biolovision/__init__.py
@@ -2,21 +2,10 @@ import gettext
 import importlib.metadata
 from pathlib import Path
 
-__version__ = "unknown"
-
 try:
     __version__ = importlib.metadata.version("Client_API_VN")
 except importlib.metadata.PackageNotFoundError:
-    # Fall back on getting it from a local pyproject.toml.
-    # This works in a development environment where the
-    # package has not been installed from a distribution.
-    import toml
-
-    pyproject_toml_file = Path(__file__).parent.parent.parent / "pyproject.toml"
-    if pyproject_toml_file.exists() and pyproject_toml_file.is_file():
-        __version__ = toml.load(pyproject_toml_file)["tool"]["poetry"]["version"]
-        # Indicate it might be locally modified or unreleased.
-        __version__ = __version__ + "+"
+    __version__ = "unknown"
 
 # Install gettext for any file in the application
 localedir = Path(__file__).resolve().parent / "locale"
