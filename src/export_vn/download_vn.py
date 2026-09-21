ef903d66a6e8682125cc25556ad86e19f8d51802
Daniel Thonon <d.thonon9@gmail.com>
2026-09-21 23:09:58 +0200

    Correction de la date de téléchargement. Bug non corrigé

--- src/export_vn/download_vn.py
+++ src/export_vn/download_vn.py
@@ -441,7 +441,7 @@ class Observations(DownloadVn):
 
     Methods
     - store               - Download (by date interval) and store to json
-    - update              - Download (by date interval) and store to json
+    - update              - Download increment from VN by API and store json to file
 
     """
 
@@ -778,9 +778,9 @@ class Observations(DownloadVn):
 
                     # Record end of download interval
                     end_date = datetime.now() if self._end_date is None else datetime.combine(self._end_date, time.min)
-                    since = self._backend.increment_get(self._site, id_taxo_group)
-                    if since is None:
-                        since = end_date
+                    # since = self._backend.increment_get(self._site, id_taxo_group)
+                    # if since is None:
+                    #     since = end_date
 
                     # When to start download interval
                     start_date = end_date
@@ -873,10 +873,10 @@ class Observations(DownloadVn):
                                 timing,
                             )
                             logger.info(log_msg)
+                        self._backend.increment_log(self._site, id_taxo_group, start_date)
                         seq += 1
                         end_date = start_date
                         delta_days = int(pid(nb_obs))
-                    self._backend.increment_log(self._site, id_taxo_group, since)
         except HTTPError:
             self._backend.log(
                 self._site,
